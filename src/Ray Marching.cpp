6116c194dd1777b9ecee1516a477b0d9e3359745
Isac's Desktop <iwandiba@hotmail.com>
2024-01-02 11:04:25 -0500

    Add project files.

---
+++ src/Ray Marching.cpp
@@ -0,0 +1,12 @@
+﻿// Ray Marching.cpp : Defines the entry point for the application.
+//
+
+#include "Ray Marching.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
